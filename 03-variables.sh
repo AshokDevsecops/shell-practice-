13206e6391f6b582d22932ce972712c2730de73a
AshokDevsecops <ashok.devsecops.ai@gmail.com>
2026-05-24 22:00:52 +0530

    converstation

---
+++ 03-variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+
+PERSON1=$1
+PERSON2=$2
+echo "$PERSON1: Hey $PERSON2, We are going to blast you"
+echo "$PERSON2: Yes, Please go ahead"
+echo "$PERSON1: Hey we are very serious"
+echo "$PERSON2: Hey $PERSON1, we are also very serious"
