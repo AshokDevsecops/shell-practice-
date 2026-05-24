0a439ea0f4667ae7376b9c401dec207c1ed78bcf
AshokDevsecops <ashok.devsecops.ai@gmail.com>
2026-05-24 22:06:25 +0530

    converstation

---
+++ 04-variables.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Please enter ur username"
+read USER_NAME #HERE USERNAME IS VARIABLE, WHATEVER YOUR ENTER IN TERMINAL THAT WILL BE STORED IN THIS VARIABLE
+
+echo " Username is  $USER_NAME "
+
+echo "Please enter ur password"
+read -s PASSWORD
+
+echo "Password is: $PASSWORD"
+ 
